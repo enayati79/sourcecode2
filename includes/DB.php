667161c80d7a202b93a7f25a402aa36c05f8ee83
enayati79 <79705811+enayati79@users.noreply.github.com>
2021-03-11 21:33:42 +0330

    Add files via upload

---
+++ includes/DB.php
@@ -0,0 +1,51 @@
+<?php
+	include 'Product.php';
+if(! class_exists('DB')) {
+	class DB{
+		var $dbc;
+		function __construct( $selectDB = true ){
+			$this -> connect();
+			if( $selectDB )
+				$this -> selectDB();
+			$this -> dbc -> set_charset( CHARSET );
+		}
+		
+		function connect(){
+			$this -> dbc = new mysqli(DBHOST, DBUSER, DBPASS); 
+			if( $this -> dbc -> connect_error ){
+				$error = "
+						خطا در اتصال به دیتابیس!
+						<section lang = 'en'>{$this -> dbc -> connect_error}</section>";
+				die( $error );
+			}
+		}
+		function selectDB(){
+			$this -> dbc -> select_db(DBNAME);
+			if( $this -> dbc -> error ){
+				$error = "
+						خطا در انتخاب دیتابیس!
+						<section lang = 'en'>{$this -> dbc -> error}</section>";
+				die( $error );
+			}
+		}
+
+		function execute( $sql, $params = false )
+		{
+			$result = $this -> dbc -> query( $sql );
+
+			if( $this -> dbc -> error ){
+				$error = "
+						خطا در اجرای کوئری!
+						<section lang = 'en'>{$sql}<br>
+						{$this -> dbc -> error}</section>";
+				die( $error );
+			}
+			else
+				return true;
+		}
+		
+		function __destruct(){
+			$this -> dbc -> close(); 
+		}
+	}
+}
